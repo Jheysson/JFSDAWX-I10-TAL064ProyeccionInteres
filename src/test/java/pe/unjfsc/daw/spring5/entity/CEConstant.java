80907c43ab69323e27abe9693a0782ab9bdb0e9d
Jheysson Urbano <jheyssonurb@gmail.com>
2020-10-07 21:18:10 -0500

    Creacion de constantes

--- src/test/java/pe/unjfsc/daw/spring5/entity/CEConstant.java
+++ src/test/java/pe/unjfsc/daw/spring5/entity/CEConstant.java
@@ -1,5 +1,13 @@
 package pe.unjfsc.daw.spring5.entity;
 
 public class CEConstant {
-
+	//TASA
+	public static final double tasaOct = 0.0570;
+	public static final double tasaNov = 0.0586;
+	public static final double tasaDic = 0.0699;
+	
+	//DIAS
+	public static final int diaOct = 31;
+	public static final int diaNov = 30;
+	public static final int diaDic = 31;
 }
